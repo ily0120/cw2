c5f721f0a191d90634ece20d83070a01e44200a3
ily0120 <ilopan0512@gmail.com>
2025-03-13 11:19:26 +0100

    zad2

--- Zad2.java
+++ Zad2.java
@@ -2,15 +2,44 @@ import java.util.HashMap;
 
 public class Zad2 {
     public static void main(String[] args) {
+        System.out.println("tab 1");
         int[] array1 = {3, 3, 3, 2, 2};
+        System.out.print("[");
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+            if (i < array1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]"+"\nwynik");
+
         System.out.println(findDominant(array1));
 
+        System.out.println("tab 2");
         int[] array2 = {1, 2, 3, 4};
+        System.out.print("[");
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i]);
+            if (i < array2.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]"+"\nwynik");
         System.out.println(findDominant(array2));
 
+        System.out.println("tab 3");
         int[] array3 = {5, 5, 5, 5, 1};
+        System.out.print("[");
+        for (int i = 0; i < array3.length; i++) {
+            System.out.print(array3[i]);
+            if (i < array3.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]"+"\nwynik");
         System.out.println(findDominant(array3));
     }
+
 
     public static int findDominant(int[] array) {
         if (array == null || array.length == 0) {
